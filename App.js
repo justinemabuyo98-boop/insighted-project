121f9743693acfaa1db739b77c247c4315969297
Jas <justinemabuyo98@gmail.com>
2026-05-01 17:21:44 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import RegionChart from "./components/RegionChart";
+import DivisionChart from "./components/DivisionChart";
+import ScatterPlot from "./components/ScatterChart";
+
+function App() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>InsightEd Dashboard</h1>
+
+      <h2>Shortage by Region</h2>
+      <RegionChart />
+
+      <h2>Division Deep Dive</h2>
+      <DivisionChart />
+
+      <h2>Enrollment vs Shortage</h2>
+      <ScatterPlot />
+
+      <h1>School Shortage Dashboard</h1>
+      <br/>
+      <p style={{ color: 'gray', fontStyle: 'italic' }}>Created by: Justine F. Mabuyo</p>
+    </div>
+    
+    
+  );
+}
+
+export default App;
